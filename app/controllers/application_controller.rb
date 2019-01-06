47603f3e38050b5976afd5e56b8002d1b0e461cf
Cheng Vue <cvu3@ucsc.edu>
2019-01-06 05:06:28 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,5 +4,14 @@ class App < Sinatra::Base
 
     set :views, Proc.new { File.join(root, "../views/") }
 
+    get "/" do
+      erb :team
+    end
+
+    post "/teams" do
+      @team = params["team"]
+
+      erb :super_hero
+    end
 
 end
