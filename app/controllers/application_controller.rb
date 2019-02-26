09db31bcd6446ce0662031f2156812f8fedc7283
Daniel Dawson <dannyldawson@gmail.com>
2019-02-26 12:36:58 -0800

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,19 @@
 require 'sinatra/base'
+require 'pry'
 
 class App < Sinatra::Base
+  set :views, proc { File.join(root, '../views/') }
 
-    set :views, Proc.new { File.join(root, "../views/") }
+  get '/' do
+    erb :super_hero
+  end
 
-
+  post '/teams' do
+    @team = Team.new(name: params[:team][:name], motto: params[:team][:motto])
+    params[:team][:member].each do |hero_hash|
+      hero = Hero.new(name: hero_hash[:name], power: hero_hash[:power], biography: hero_hash[:bio])
+      @team.add_hero(hero)
+    end
+    erb :team
+  end
 end
