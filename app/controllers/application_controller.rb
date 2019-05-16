94085e7dd6b71cd34402337424ddb0de8aee145c
Blaine Lawson <blaine.lawson@gmail.com>
2019-05-16 15:10:13 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,5 +4,14 @@ class App < Sinatra::Base
 
     set :views, Proc.new { File.join(root, "../views/") }
 
+    get '/' do
+      erb :'../views/super_hero'
+    end
 
+    post '/teams' do
+      # binding.pry
+      @team = params[:team]
+# binding.pry
+      erb :'../views/team'
+    end
 end
