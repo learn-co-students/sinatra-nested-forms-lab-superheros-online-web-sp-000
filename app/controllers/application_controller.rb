151c72b338c24568a2b5d132cfc45493376e28cd
Yingqi Chen <chanwk18@hotmail.com>
2019-06-29 19:05:30 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,11 +9,13 @@ class App < Sinatra::Base
     end
 
     post '/team' do
-      @team=Team.new( params[:team][:name],params[:team][:motto])
+
+      @team=Team.new(params[:team])
       @hero_1=Hero.new(params[:member1_name],params[:member1_power],params[:member1_bio])
       @hero_2=Hero.new(params[:member2_name],params[:member2_power],params[:member2_bio])
       @hero_3=Hero.new(params[:member3_name],params[:member3_power],params[:member3_bio])
       erb :team
+
     end
 
 
--- app/models/hero.rb
+++ app/models/hero.rb
@@ -2,10 +2,10 @@ class Hero
 
   attr_accessor :name, :power, :bio
 
-  def initialize(args)
-    @name=args[:name]
-    @power=args[:power]
-    @bio=args[:bio]
+  def initialize(name,power,bio)
+    @name=name
+    @power=power
+    @bio=bio
   end
 
 end
