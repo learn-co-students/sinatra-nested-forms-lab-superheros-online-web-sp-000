0fc7489b91097a98248ebce0d79d9d4d9b75ead0
Kirill Shcherbina <rabbichaim770@gmail.com>
2019-09-04 01:40:29 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,19 @@
+require 'pry'
 require 'sinatra/base'
 
 class App < Sinatra::Base
 
     set :views, Proc.new { File.join(root, "../views/") }
 
+
+  get '/' do
+    erb :super_hero
+  end
+
+  post '/team' do
+  @heroes = params[:team]
+  # binding.pry
+  erb :team
+  end
 
 end
