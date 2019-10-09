fcd795e7f1544aac7abe38f93e03d2e34b9a305f
Michael McCullough <michael.mccullough21@gmail.com>
2019-10-09 14:18:07 +0000

    Done.

---
+++ app/models/team.rb
@@ -0,0 +1,15 @@
+class Team
+  attr_accessor :name, :motto
+  @@all = []
+
+  def initialize(params)
+    @motto = params[:motto]
+    @name = params[:name]
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+end
