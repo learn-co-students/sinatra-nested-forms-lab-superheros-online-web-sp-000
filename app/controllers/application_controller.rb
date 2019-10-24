c7c975bb58a74aa6baef7734456ba0d0e6051bc5
Bernadette Davis <bsd231@nyu.edu>
2019-10-24 19:18:26 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,5 +4,24 @@ class App < Sinatra::Base
 
     set :views, Proc.new { File.join(root, "../views/") }
 
+    get '/' do
+      erb :super_hero
+    end
+
+    post '/teams' do
+      @params = params
+=begin
+      @new_team = Pirate.new(params["pirate"]["name"], params["pirate"]["weight"], params["pirate"]["height"])
+
+      params[:pirate][:ships].each do |ship|
+        Ship.new(ship)
+      end
+
+      @ships = Ship.all
+      #params["team"].to_s
+      #{}"Team Motto:" + params["team"].to_s
+=end
+      erb :team
+    end
 
 end
