ff7d5eedab76b354849cc2057e16baa48ca092c6
Brian Pompey <brianthescholar@gmail.com>
2019-12-10 00:14:47 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -19,6 +19,7 @@ class App < Sinatra::Base
       @heroes = params[:team][:heroes]
 
       @heroes.each do |hero|
+#        binding.pry
         @hero_name << hero[:name]
         @hero_power << hero[:power]
         @hero_bio << hero[:biography]
