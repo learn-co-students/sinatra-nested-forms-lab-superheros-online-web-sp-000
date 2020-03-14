e7b8130e134c53454e3f6d56b013afd23bc9f358
CoderJay06 <jaycru06@yahoo.com>
2020-03-14 01:32:41 -0400

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -14,14 +14,14 @@ class App < Sinatra::Base
         @team_motto = params["team"]["motto"]
         @member1_name = params["team"]["members"][0]["name"]
         @member1_power = params["team"]["members"][0]["power"]
-        @member1_biography = params["team"]["members"][0]["biography"]
+        @member1_bio = params["team"]["members"][0]["biography"]
         @member2_name = params["team"]["members"][1]["name"]
         @member2_power = params["team"]["members"][1]["power"]
-        @member2_biography = params["team"]["members"][1]["biography"]
+        @member2_bio = params["team"]["members"][1]["biography"]
         @member3_name = params["team"]["members"][2]["name"]
         @member3_power = params["team"]["members"][2]["power"]
-        @member3_biography = params["team"]["members"][2]["biography"]
-        
+        @member3_bio = params["team"]["members"][2]["biography"]
+
         erb :team 
     end 
 
