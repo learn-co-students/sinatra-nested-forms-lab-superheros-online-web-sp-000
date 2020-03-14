d48885c805c4896638c40cd4bcbca7320aff4246
CoderJay06 <jaycru06@yahoo.com>
2020-03-13 22:21:47 -0400

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,5 +4,9 @@ class App < Sinatra::Base
 
     set :views, Proc.new { File.join(root, "../views/") }
 
+    # make a route that responds to a GET request at '/'
+    get '/' do 
+        erb :super_hero
+    end 
 
 end
