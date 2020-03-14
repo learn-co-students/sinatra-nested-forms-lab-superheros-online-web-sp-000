ceeb481f4c0261d44575dd696ac4b69d404a1095
CoderJay06 <jaycru06@yahoo.com>
2020-03-14 00:51:32 -0400

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,10 +6,14 @@ class App < Sinatra::Base
 
     # make a route that responds to a GET request at '/'
     get '/' do 
+        binding.pry
         erb :super_hero
     end 
 
-    post '/teams' do 
+    post '/teams' do  
+        @team_name = params["team"]["name"] 
+        @team_motto = params["team"]["motto"]
+        @member1_name = params["team"]["members"][]["name"]
         erb :team 
     end 
 
