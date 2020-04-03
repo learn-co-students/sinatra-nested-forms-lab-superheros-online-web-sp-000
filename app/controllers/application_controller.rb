c79b87cc1dcaaa47e766c6c906251f6b4d013c7f
Jasmine Carter <jasmine.carter87@gmail.com>
2020-04-03 00:11:30 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,5 +4,24 @@ class App < Sinatra::Base
 
     set :views, Proc.new { File.join(root, "../views/") }
 
+    get '/' do
+      erb :create_team
+    end
+
+    post '/teams' do
+      #binding.pry
+      @team_name = params[:team][:name]
+      @team_motto = params[:team][:motto]
+      @member1_name = params[:team][:heroes][0][:name]
+      @member1_power = params[:team][:heroes][0][:power]
+      @member1_biography = params[:team][:heroes][0][:biography]
+      @member2_name = params[:team][:heroes][1][:name]
+      @member2_power = params[:team][:heroes][1][:power]
+      @member2_biography = params[:team][:heroes][1][:biography]
+      @member3_name = params[:team][:heroes][2][:name]
+      @member3_power = params[:team][:heroes][2][:power]
+      @member3_biography = params[:team][:heroes][2][:biography]
+      erb :team
+    end
 
 end
