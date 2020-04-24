8c2ef2e6afd9ca0b21cdbefccaaa890389923ecb
Chad Lengefeld <chadlengefeld@gmail.com>
2020-04-24 18:15:56 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,36 +6,28 @@ class App < Sinatra::Base
 
     get '/' do
 
-        erb :"/team"
+        erb :"/super_hero"
     end
 
-    # get '/team' do
-    #   erb :team
-    # end
+
+
+
 
     post '/teams' do
-      @team = params
+      #  binding.pry
       @team_name = params[:team][:name]
       @motto = params[:team][:motto]
-      @member1_name = params[:team][:hero][0][:name]
-      @member1_power = params[:team][:hero][0][:power]
-      @member1_bio = params[:team][:hero][0][:biography]
+      @hero_name = []
+      @hero_power = []
+      @hero_bio = []
 
-      @member2_name = params[:team][:hero][1][:name]
-      @member2_power = params[:team][:hero][1][:power]
-      @member2_bio = params[:team][:hero][2][:biography]
+      @heroes = params[:team][:hero]
+      @heroes.each do |hero|
 
-      @member3_name = params[:team][:hero][2][:name]
-      @member3_power = params[:team][:hero][2][:power]
-      @member3_bio = params[:team][:hero][2][:biography]
-      #  binding.pry
-      erb :team
+        @hero_name << hero[:name]
+        @hero_power << hero[:power]
+        @hero_bio << hero[:bio]
+      end
+        erb :teams
     end
-
-    #
-    get '/teams' do
-
-      erb :team
-    end
-
-end
+ end
