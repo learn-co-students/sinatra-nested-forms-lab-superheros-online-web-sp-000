52638e5748c02c350ad1c426bac6c3ad48ecd5c8
dkw5011 <dkw5011@gmail.com>
2020-04-24 22:20:46 -0400

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,7 +9,18 @@ get '/' do
 end
 
 post '/teams' do
-    @teams = params["team"]
+    @team_name = params[:team][:name]
+    @team_motto = params[:team][:motto]
+    @hero_name = []
+    @hero_power = []
+    @hero_bio = []
+    @team_members = params[:team][:members]
+    @team_members.each do |hero|
+        # binding.pry
+        @hero_name << hero[:name]
+        @hero_power << hero[:power]
+        @hero_bio << hero[:bio]
+end
     erb :team
 end
 end
