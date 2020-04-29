2197d88d80bfdee7030f9888932767a59db86de9
jkellyphilly <joel.kelly.philly@gmail.com>
2020-04-29 14:41:47 -0400

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,6 +10,7 @@ class App < Sinatra::Base
 
     post "/teams" do
       @team_name = params[:team][:name]
+      @team_motto = params[:team][:motto]
       @hero_1 = params[:team][:members][0]
       @hero_2 = params[:team][:members][1]
       @hero_3 = params[:team][:members][2]
