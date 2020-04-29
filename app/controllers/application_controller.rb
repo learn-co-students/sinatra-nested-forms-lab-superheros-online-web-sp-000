ac45b65da03004b5469b6a7f06c5cb566bb5115f
jkellyphilly <joel.kelly.philly@gmail.com>
2020-04-29 12:07:06 -0400

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,4 +8,13 @@ class App < Sinatra::Base
       erb :super_hero
     end
 
+    post "/teams" do
+      @team_name = params[:team][:name]
+      @hero_1 = params[:team][:members][0]
+      @hero_2 = params[:team][:members][1]
+      @hero_3 = params[:team][:members][2]
+
+      erb :team
+    end
+
 end
