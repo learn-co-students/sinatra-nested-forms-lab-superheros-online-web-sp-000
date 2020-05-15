50d7912ee6fca76c4681581c9cbd8e17b9c92e54
Joseph DeGregorio <DeGregorio.Joseph@Gmail.com>
2020-05-14 20:24:39 -0400

    Done.

---
+++ app/models/heros.rb
@@ -0,0 +1,22 @@
+class Hero
+    attr_accessor :name, :power, :biography
+    @@all = []
+    
+    def initialize (args)
+        @name = args[:name]
+        @power = args[:power]
+        @biography = args[:biography]
+        save
+    end
+
+    def save
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+    def self.clear
+        @@all.clear
+    end
+end
