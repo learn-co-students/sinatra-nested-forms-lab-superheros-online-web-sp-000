f87fc4e3a06dbbc678debb9ac80ac765c619f6ef
anthony mcmullen <jondouglasheit@gmail.com>
2020-05-29 16:20:46 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,23 @@
 require 'sinatra/base'
+require 'pry'
 
 class App < Sinatra::Base
 
     set :views, Proc.new { File.join(root, "../views/") }
 
+  get '/' do 
+    erb :super_hero
+  end 
+  
+  post '/team' do 
+  @team = Team.new(params[:team])
+  
+  params[:team][:super_hero].each do |details|
+  binding.pry
+    @super_hero = Super_hero.new(
+      details)
+    end
+      erb :team
+  end 
 
 end
