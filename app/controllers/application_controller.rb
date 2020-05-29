26ac25813a2f1556ba3bc8e31e1fc9bde7208fd5
anthony mcmullen <jondouglasheit@gmail.com>
2020-05-29 19:59:46 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,11 +11,12 @@ class App < Sinatra::Base
 
   post '/teams' do
       @team = Team.new(params[:team])
+
       params[:team][:members].each do |details| 
-        binding.pry
         SuperHero.new(details)
       end
       @members = SuperHero.all
       erb :teams
     end  
+
 end
