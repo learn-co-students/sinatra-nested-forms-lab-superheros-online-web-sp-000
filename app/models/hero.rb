c3416416687fd7c986fd32fbb80decf3b7b16de3
jackcooper1245 <jackcooper1245@gmail.com>
2020-06-27 16:31:45 +0200

    Done.

---
+++ app/models/hero.rb
@@ -0,0 +1,17 @@
+class Hero
+
+  attr_accessor :name, :power, :bio
+
+  @@all = []
+
+  def initialize(name, power, bio)
+    @name = name
+    @power = power
+    @bio = bio
+  end
+
+  def self.all
+    @all
+  end
+
+end 
