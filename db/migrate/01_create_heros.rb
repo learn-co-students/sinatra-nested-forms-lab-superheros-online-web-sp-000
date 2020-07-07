68837433da0f391b08d71e9688028cd346bbbfb3
AustinRhoads <austinrrhoads@ymail.com>
2020-07-06 21:25:47 -0500

    Done.

---
+++ db/migrate/01_create_heros.rb
@@ -0,0 +1,10 @@
+class CreateHeros < ActiveRecord::Migration[5.2]
+    def change
+        create_table :heros do |t|
+            t.string :name
+            t.string :power
+            t.string :biography
+            t.integer :team_id
+        end
+        end
+end
