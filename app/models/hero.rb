fa6b7dffbe2f37230865218b740a9806fca314fd
Harley Hanna <harleyhannawrit@gmail.com>
2020-10-26 15:51:50 +0000

    Done.

---
+++ app/models/hero.rb
@@ -0,0 +1,17 @@
+class Hero
+  
+  attr_accessor :name, :power, :bio
+  
+  HEROES = []
+  
+  def initialize(hash)
+    @name = hash[:name]
+    @power = hash[:power]
+    @bio = hash[:bio]
+    HEROES << self
+  end
+  
+  def self.all
+    HEROES
+  end
+end 
---
+++ app/models/team.rb
@@ -0,0 +1,17 @@
+class Team
+  
+  attr_accessor :name, :motto
+  
+  TEAMS = []
+  
+  def initialize(hash)
+    @name = hash[:name]
+    @motto = hash[:motto]
+    TEAMS << self
+  end
+  
+  def self.all
+    TEAMS
+  end 
+  
+end
