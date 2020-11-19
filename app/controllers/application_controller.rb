db22a7644e0efd340cab5bacce676d5c15dfe0e8
rmholsen <jaguar.kitty@gmail.com>
2020-11-19 18:24:09 -0500

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,5 +4,14 @@ class App < Sinatra::Base
 
     set :views, Proc.new { File.join(root, "../views/") }
 
+get '/' do
+    erb :super_hero
+end 
+
+post '/teams' do
+    @team_name = params[:name]
+    @team_motto = params[:motto]
+    erb :team
+end 
 
 end
