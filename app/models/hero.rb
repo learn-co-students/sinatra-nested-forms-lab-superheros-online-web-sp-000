6888a1da289ab4e8c5c192f3371449019fe76e1a
nllew <n.lewis.perc@gmail.com>
2021-01-01 23:50:34 -0600

    Done.

---
+++ app/models/hero.rb
@@ -0,0 +1,20 @@
+class Hero
+    attr_accessor :name, :power, :biography
+
+    @@all = []
+
+    def initialize(name, power, biography)
+        @name = name
+        @power = power
+        @biography = biography
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def self.clear
+        @@all.clear
+    end
+end
