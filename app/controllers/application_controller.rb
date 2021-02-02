0cc5e927eba0e7175c45a435bebbbc6a71124821
abstractjazz <evan@heyevan.com>
2021-02-01 23:04:24 -0800

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,6 +7,21 @@ class App < Sinatra::Base
     get '/' do 
         erb :new 
     end 
+
+    post '/teams' do 
+        @team = Team.new(params[:team])
+        
+            
+            params[:team][:members].each do |details|
+            Superhero.new(details)
+            end 
+
+            @superheros = Superhero.all
+        # binding.pry
+           
+        erb :show
+        # raise params.inspect
+    end 
 end
 
 
--- app/views/new.rb
+++
@@ -1,8 +0,0 @@
-
-<form action='/teams' method='POST'>
-
-<label for="team_name">Team Name:</label> 
-<input type="text" name="team[name]" id="team_name">
-<input type="text" name="team[motto]" id="team_motto">
-<input type="submit">
-</form>
