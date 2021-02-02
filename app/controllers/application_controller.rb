b8f66ba19e5950b546f0862414cefd7dd24b0989
torpedo-t <55634516+torpedo-t@users.noreply.github.com>
2021-02-02 13:14:43 -0600

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,22 @@
 require 'sinatra/base'
-
+require 'pry'
 class App < Sinatra::Base
 
     set :views, Proc.new { File.join(root, "../views/") }
 
+    get '/' do
+        #binding.pry
+        erb :team
+    end
 
+    post '/teams' do
+        #binding.pry
+        @team = Team.new(params[:team])
+        #binding.pry
+        params[:team][:heroes].each do |details|
+            Hero.new(details)
+        end
+        @heroes = Hero.all
+        erb :super_hero
+    end
 end
