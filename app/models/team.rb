cad3594776d5f51efafd7b56abee4a03832156aa
Christerle Nounoute <christerlesn@gmail.com>
2021-02-10 17:17:31 +0000

    Done.

--- app/models/team.rb
+++ app/models/team.rb
@@ -1,15 +1,10 @@
 class Team
-  attr_reader :name, :motto
 
-  TEAMS = []
+  attr_reader :name, :motto
 
   def initialize(params)
     @name = params[:name]
     @motto = params[:motto]
-    TEAMS << self
   end
 
-  def self.all
-    TEAMS
-  end
 end
