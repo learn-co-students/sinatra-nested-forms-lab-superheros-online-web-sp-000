8843abe6b83cb7f0f722b4838abcd688ae3d078b
Chandler Craig <hellochandlercraig@gmail.com>
2021-02-27 21:26:42 -0500

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,5 +4,19 @@ class App < Sinatra::Base
 
     set :views, Proc.new { File.join(root, "../views/") }
 
+    get '/' do
+        erb :super_hero
+    end
+  
+    post '/team' do
+        "hello"
+        @team = Team.new(params[:team])
+        members = params[:team][:members]
+        @heros = members.collect do |hash|
+          SuperHero.new(hash)
+        end
+        erb :team
+    end
+
 
 end
