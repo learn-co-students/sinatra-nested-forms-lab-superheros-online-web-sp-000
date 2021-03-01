90dcc67a560435fbc9bf115dd9397a737dfec686
dbrandt1990 <dvbrandt90@gmail.com>
2021-02-28 19:19:57 -0700

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,9 +10,9 @@ class App < Sinatra::Base
 
     post '/team' do 
         @team = Team.new(params[:team][:name], params[:team][:motto])
-        @hero1 = Hero.new(params[:team][:members][0][:name], params[:team][:members][0][:power], params[:team][:members][0][:bio]) 
-        @hero2 = Hero.new(params[:team][:members][1][:name], params[:team][:members][1][:power], params[:team][:members][1][:bio]) 
-        @hero3 = Hero.new(params[:team][:members][2][:name], params[:team][:members][2][:power], params[:team][:members][2][:bio]) 
+        Hero.new(params[:team][:members][0][:name], params[:team][:members][0][:power], params[:team][:members][0][:bio]) 
+        Hero.new(params[:team][:members][1][:name], params[:team][:members][1][:power], params[:team][:members][1][:bio]) 
+        Hero.new(params[:team][:members][2][:name], params[:team][:members][2][:power], params[:team][:members][2][:bio]) 
 
         erb :team
     end 
