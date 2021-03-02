d1e1c4bfa0dcd61134f14760e19cce260162e8ad
jasonronalddavis <jdavis@c.ringling.edu>
2021-03-02 05:57:26 -0600

    Done.

---
+++ app/models/hero.rb
@@ -0,0 +1,22 @@
+
+class Hero 
+    attr_accessor :name, :power, :bio 
+
+    @@all = []
+
+    def initialize(name, power, bio)
+        @name = name 
+        @power = power 
+        @bio = bio 
+        @@all << self
+    end
+
+    def self.all 
+        @@all
+    end
+
+    def self.clear 
+        @@all = []
+    end
+
+end 
