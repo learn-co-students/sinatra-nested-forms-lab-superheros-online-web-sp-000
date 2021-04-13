cd619af46bfb0f6aba41862ce0f6c3e67e061fec
remote-torrent-6747 <abbyc@sky.com>
2021-04-13 21:46:35 +0000

    Done.

---
+++ app/models/team.rb
@@ -0,0 +1,16 @@
+# AC CREATED
+class Team
+  attr_accessor :name, :motto
+
+  @@all = []
+  
+  def initialize(params)
+    @name = params[:name]
+    @motto = params [:motto]
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+end
