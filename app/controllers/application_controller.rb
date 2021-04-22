d7c2dbe3487414df71998e0a1bc4bf25237b61e7
Firooz Mafakheri <firooz.mafakheri@gmail.com>
2021-04-22 21:40:05 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,6 +11,7 @@ class App < Sinatra::Base
 
     post '/teams' do
       @team = Team.new(name: params[:team][:name], motto: params[:team][:motto])
+      
       members = params[:team][:members]
 
       members.each do |member_params|
